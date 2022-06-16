46b52b136bcaefb225d688330428a15625ac5121
Poonyawat Suntiniwat <ninekrub139@gmail.com>
2022-06-16 14:14:02 +0700

    small fix

--- Assets/Script/ScriptableStructure/enemyHouse.cs
+++ Assets/Script/ScriptableStructure/enemyHouse.cs
@@ -17,17 +17,20 @@ public class enemyHouse : MonoBehaviour
     private float rangeToDetected;
 
     private Transform goblinToDetected;
+    private Transform enemyRoot;
     // Start is called before the first frame update
     void Start()
     {
         goblinToDetected = GameObject.Find("PlayingGoblin").GetComponent<Transform>();
+        enemyRoot = GameObject.Find("Enemy").transform;
     }
 
     void Update()
     {
         if(Vector3.Distance(goblinToDetected.position,whereIsDoor.position) <= rangeToDetected && isHostInTheHouse)
         {
-            Instantiate(hostPrefab,whereIsDoor.position,whereIsDoor.rotation);
+            GameObject GO = Instantiate(hostPrefab,whereIsDoor.position,new Quaternion(0,180,0,0));
+            GO.transform.SetParent(enemyRoot);
             isHostInTheHouse = false;
         }
     }
