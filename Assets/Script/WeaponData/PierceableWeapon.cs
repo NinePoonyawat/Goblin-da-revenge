7b8d71921debc49dd984632836c2820435506b38
inwbear <kaninsk139@gmail.com>
2022-06-18 22:42:37 +0700

    1

---
+++ Assets/Script/WeaponData/PierceableWeapon.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon/MeleeWeapon/PierceableWeapon")]
+public class PierceableWeapon : MeleeWeapon
+{
+    
+}
